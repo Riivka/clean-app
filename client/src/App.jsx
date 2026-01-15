bc8c43412d195d3d4d715d1005387cf7b3dfa250
Riivka <rivkaover@gmail.com>
2026-01-15 13:09:37 +0200

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count} hello yes
+          count is {count} hello yes world
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
